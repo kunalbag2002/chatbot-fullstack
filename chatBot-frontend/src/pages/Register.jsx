81f1013c15a60b2b6fd88481e35e8fc490017eb0
kunalbag2002 <93586480+kunalbag2002@users.noreply.github.com>
2025-11-13 12:33:39 +0530

    Update Register.jsx

--- chatBot-frontend/src/pages/Register.jsx
+++ chatBot-frontend/src/pages/Register.jsx
@@ -16,7 +16,7 @@ export default function Register() {
                     onSubmit={async (e) => {
                         e.preventDefault();
 
-                        const res = await fetch("http://127.0.0.1:8000/register", {
+                        const res = await fetch("https://chatbot-fullstack-j8hr.onrender.com/register", {
                             method: "POST",
                             headers: { "Content-Type": "application/json" },
                             body: JSON.stringify({
